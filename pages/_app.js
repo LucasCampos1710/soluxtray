2813c9027c554fba6af4c8723cfec2e883f8ba9b
LucasCampos1710 <soluxcreative@gmail.com>
2022-08-29 16:31:49 -0300

    Add facebook-pixel

--- pages/_app.js
+++ pages/_app.js
@@ -3,26 +3,57 @@ import Script from 'next/script'
 import { useRouter } from 'next/router'
 import { GTM_ID, pageview } from '../lib/gtm'
 import * as gtag from '../lib/gtag'
+import * as fbq from '../lib/fpixel'
 
 import 'tailwindcss/tailwind.css'
 import './styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url)
+
+      fbq.pageview()
+
+      const handleRouteChange = () => {
+        fbq.pageview()
+      }
     }
-    router.events.on('routeChangeComplete', pageview)
+    router.events.on('routeChangeComplete', pageview, handleRouteChange)
     router.events.on('hashChangeComplete', handleRouteChange)
     return () => {
-      router.events.off('routeChangeComplete', pageview)
+      router.events.off('routeChangeComplete', pageview, handleRouteChange)
       router.events.off('hashChangeComplete', handleRouteChange)
     }
   }, [router.events])
 
   return (
     <>
+     {/* Meta Pixel Code */}
+      <script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){ n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments) };
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', ${fbq.FB_PIXEL_ID});
+        fbq('track', 'PageView');
+      </script>
+     
+        {/* End Meta Pixel Code */}
+          `
+        }}
+      />
+       
       {/* <!-- Google Tag Manager --> */ }
       <Script
         id="gtag-base"
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { FB_PIXEL_ID } from '../lib/fpixel'
 import { GTM_ID } from '../lib/gtm'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <noscript>
+            <img 
+            height="1" 
+            width="1" 
+            tyle="display:none"
+          src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+          </noscript>
+        </Head>
         <body>
           {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript>
