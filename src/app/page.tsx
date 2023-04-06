5dc924dd745724bf834b9e9a17422fb7ffa743ab
LucasCampos1710 <soluxcreative@gmail.com>
2023-04-06 20:40:05 -0300

    fix: text page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             <main className="mt-10 pt-7 mx-auto max-w-7xl px-4 sm:mt-20 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
-                  <span className="block xl:inline uppercase">Somos especialistas na criação </span>{' '}
+                  <span className="block xl:inline uppercase">Somos especialistas em criação </span>{' '}
                   <span className="block text-violet-600 xl:inline uppercase">de lojas virtuais de alta performance.</span>
                 </h1>
                 <div className=" flex pt-10 text-base font-semibold text-gray-900 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
